20adc2ac0a704492dcfdde2550f5d8c5124a83a5
mayank bhatnagar <manumeet.bhatnagar1@gmail.com>
2026-01-31 11:38:00 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3274070059-be589",
+  "appId": "1:797045355004:web:4f740fcc3af386eee64279",
+  "apiKey": "AIzaSyC0LZf8syk5PXu7SO8hLmfBtdv8d6TIRTI",
+  "authDomain": "studio-3274070059-be589.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "797045355004"
+};

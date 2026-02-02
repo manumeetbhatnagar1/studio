6087cc99036e2f9bc2fb7307b5b15a791ff88b92
mayank bhatnagar <manumeet.bhatnagar1@gmail.com>
2026-02-02 21:43:37 +0000

    Try fixing this error: `Runtime ReferenceError: useEffect is not defined

--- src/app/(main)/admin/students/page.tsx
+++ src/app/(main)/admin/students/page.tsx
@@ -1,6 +1,5 @@
-
 'use client';
-import { useState, useMemo, FC } from 'react';
+import { useState, useMemo, FC, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';

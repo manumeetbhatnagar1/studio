94b4544a76a629b250bdbe0deca9a334e9ea486c
mayank bhatnagar <manumeet.bhatnagar1@gmail.com>
2026-02-03 04:04:56 +0000

    i have added icon image to public/icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <meta name="theme-color" content="#2563eb" />
+        <meta name="theme-color" content="#3B82F6" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link

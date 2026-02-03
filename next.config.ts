1568a83b4ff6c69e0d087f006c26b3fc57571509
mayank bhatnagar <manumeet.bhatnagar1@gmail.com>
2026-02-03 11:08:00 +0000

    go ahead

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
 import type {NextConfig} from 'next';
 import withPWAInit from '@ducanh2912/next-pwa';
+import runtimeCaching from 'next-pwa/cache';
 
 const withPWA = withPWAInit({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
   register: true,
   skipWaiting: true,
+  runtimeCaching,
 });
 
 const nextConfig: NextConfig = {

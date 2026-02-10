2907d2191d3aa1c6ba4147f0a28524a199279acb
mayank bhatnagar <manumeet.bhatnagar1@gmail.com>
2026-02-10 17:46:57 +0000

    error in sending images in chat , no default bucket found

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-3274070059-be589",
   "appId": "1:797045355004:web:4f740fcc3af386eee64279",
+  "storageBucket": "studio-3274070059-be589.appspot.com",
   "apiKey": "AIzaSyC0LZf8syk5PXu7SO8hLmfBtdv8d6TIRTI",
   "authDomain": "studio-3274070059-be589.firebaseapp.com",
   "measurementId": "",
